77ac87632ab2206339d72b238aee3002d7e55620
Nithil Eshwar <102175607+Nithil3007@users.noreply.github.com>
2025-09-28 00:22:00 -0400

    Delete admin_portal_using_database_schema directory

--- admin_portal_using_database_schema/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app';
-import 'antd/dist/reset.css';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
